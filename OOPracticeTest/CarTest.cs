eb4617259c2a675ab6860e90f3901fc16c060f01
hdeng <hdeng10@slb.com>
2022-10-27 11:33:47 +0800

    'feat:return_message_when_speed_given_car_name_and_speed'

---
+++ OOPracticeTest/CarTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPractice;
+using Xunit;
+
+namespace OOPracticeTest
+{
+    public class CarTest
+    {
+        [Fact]
+        public void Should_return_message_when_speed_given_car_name_and_speed()
+        {
+            //given
+            Car car = new Car("Cool Car", 60);
+            //when
+            string message = car.SpeedUp();
+            //then
+            Assert.Equal("Cool Car: speed up 60 km/h", message);
+        }
+    }
+}
